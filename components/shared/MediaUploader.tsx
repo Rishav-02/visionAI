cb1ce7cda84ff65acde54482c72d17014d663ea0
Rishav Raj <rishavsinha380@gmail.com>
2025-08-27 22:03:28 +0530

    Change the Clodinary preset name

--- components/shared/MediaUploader.tsx
+++ components/shared/MediaUploader.tsx
@@ -53,7 +53,7 @@ const MediaUploader = ({
 
   return (
     <CldUploadWidget
-      uploadPreset="jsm_imaginify"
+      uploadPreset="vision-ai"
       options={{
         multiple: false,
         resourceType: "image",
